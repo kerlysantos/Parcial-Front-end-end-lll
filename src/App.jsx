696bb5f9fd4db276f58bbd24db6d9dec67fe57b5
kerlysantos <santoskerly48@gmail.com>
2023-03-08 11:24:06 -0300

    preparando-app-pre-parcial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Card/Card'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1> Parcial </h1>
+      <form> </form>
+      < Card />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/Card/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <div> 
+        <h1> hola, soy una tarjeta</h1>
+    </div>
+  )
+}
+export default Card;
